861e597ab8be34d156b5eaabb0fc1b9b9c87b9e5
SujibShrestha <sujeeb98@gmail.com>
2026-09-17 16:49:08 +0545

    Refactor Blog model status field to use CharField with choices and update admin configuration

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,6 +4,9 @@ from .models import Category,Blog
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)} # autoamatically generate text based on the specific attribute
+    list_display = ('title','category','author','status','is_featured')
+    search_fields = ('id','title','category__category_name','status')
+    list_editable = ('is_featured','status')
 
 
 admin.site.register(Category)
