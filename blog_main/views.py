ecf110080c7f676de7533c1e4621fd6dddf87838
SujibShrestha <sujeeb98@gmail.com>
2026-09-21 15:59:53 +0545

    Add login and logout functionality with corresponding views and templates

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,8 +1,9 @@
 
 from django.shortcuts import redirect, render
 from blog_main.forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
 from blogs.models import Category,Blog
-
+from django.contrib.auth import authenticate, login as auth_login ,logout as auth_logout
 def home(request):
     categories = Category.objects.all()
     featured_post = Blog.objects.filter(is_featured = True,status ="Published").order_by('updated_at')
@@ -31,3 +32,27 @@ def register(request):
         'form': form
     }
     return render(request,'register.html',context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request,'login.html',context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('home')
